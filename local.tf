0a061dad9031aaa151c5373828fcce5ec5918bc7
rjmsas <Rahul.Maurya@connectwise.com>
2021-12-09 14:11:49 -0500

    added extra configuration

--- local.tf
+++ local.tf
@@ -1,5 +1,10 @@
 resource "local_file" "pet" {
-    filename = "/root/pets.txt"
+    filename = "/root/rahul/pets.txt"
     content = "We love Pets"
     file_permission = "700"
+}
+
+resource "local_file" "cat" {
+    filename = "/root/rahul/cat.txt"
+    content = "We love Cats"
 }
